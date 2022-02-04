e41320e8b5da9d19b945fa879e7927740de49000
mattlevine22 <mattlevine22@gmail.com>
2022-02-03 16:54:22 -0800

    submit parallel jobs

--- hpc_scripts/run1_inferIC.sh
+++ hpc_scripts/run1_inferIC.sh
@@ -4,7 +4,7 @@
 
 #SBATCH --time=12:00:00   # walltime
 #SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
-#SBATCH --nodes=1   # number of nodes
+#SBATCH --nodes=2   # number of nodes
 #SBATCH --mem-per-cpu=1G   # memory per CPU core
 #SBATCH -J "test0"   # job name
 
@@ -12,5 +12,6 @@ module load python3/3.7.0
 module load gcc/9.2.0
 #conda init bash
 #conda activate fno
-python3 test_run_v4_hpc.py --hpc 1 --known_inits 0 --output_dir test_unknownIC_normalizeX
-python3 test_run_v4_hpc_hybrid.py --hpc 1 --known_inits 0 --output_dir test_unknownIC_normalizeX_hybrid
+srun --ntasks=1 python3 test_run_v4_hpc.py --hpc 1 --known_inits 0 --output_dir test_unknownIC_normalizeX &
+srun --ntasks=1 python3 test_run_v4_hpc_hybrid.py --hpc 1 --known_inits 0 --output_dir test_unknownIC_normalizeX_hybrid &
+wait
