452dbd8dc1fc8747c4826a1997e3d5b198b43b34
mattlevine22 <mattlevine22@gmail.com>
2022-02-03 16:28:38 -0800

    add hpc hybrid run

---
+++ hpc_scripts/run1_hybrid.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+
+#SBATCH --time=12:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=1G   # memory per CPU core
+#SBATCH -J "test0"   # job name
+
+module load python3/3.7.0
+module load gcc/9.2.0
+#conda init bash
+#conda activate fno
+python3 test_run_v4_hpc_hybrid.py --hpc 1
