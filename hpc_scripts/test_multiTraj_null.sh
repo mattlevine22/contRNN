00dba8b8a0ce1097f9959c15848e332a46408f18
mattlevine22 <mattlevine22@gmail.com>
2022-02-14 11:57:03 -0800

    bugfix

--- hpc_scripts/test_multiTraj_null.sh
+++ hpc_scripts/test_multiTraj_null.sh
@@ -15,5 +15,5 @@ module load gcc/9.2.0
 
 round_dir="run3_smallerModels_UNKNOWN_ICs_longer_MULTITRAJNULL"
 echo "Sending results to $round_dir"
-srun --ntasks=1 python3 test_run_v6_hpc.py --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 0 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_multiTrajNull"
+srun --ntasks=1 python3 test_run_v6_hpc_multiTraj.py --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 0 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_multiTrajNull"
 wait
