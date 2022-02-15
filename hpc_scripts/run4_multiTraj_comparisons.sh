f5a758ebf10bc4bdb56338fba36aa98c254b6599
mattlevine22 <mattlevine22@gmail.com>
2022-02-14 16:35:26 -0800

    including knownIC experiments in multiTraj learning

--- hpc_scripts/run4_multiTraj_comparisons.sh
+++ hpc_scripts/run4_multiTraj_comparisons.sh
@@ -3,8 +3,8 @@
 #Submit this script with: sbatch thefilename
 
 #SBATCH --time=72:00:00   # walltime
-#SBATCH --ntasks=4   # number of processor cores (i.e. tasks)
-#SBATCH --nodes=4   # number of nodes
+#SBATCH --ntasks=6   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=6   # number of nodes
 #SBATCH --mem-per-cpu=1G   # memory per CPU core
 #SBATCH -J "test0"   # job name
 
@@ -15,6 +15,8 @@ module load gcc/9.2.0
 
 round_dir="run4_multiTraj_comparisons"
 echo "Sending results to $round_dir"
+srun --ntasks=1 python3 test_run_v6_hpc.py --match_endpoints 0 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 1 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_orig_knownIC" &
+srun --ntasks=1 python3 test_run_v6_hpc_multiTraj.py --match_endpoints 0 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 1 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_multiTraj_knownIC" &
 srun --ntasks=1 python3 test_run_v6_hpc.py --match_endpoints 0 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 0 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_orig" &
 srun --ntasks=1 python3 test_run_v6_hpc_multiTraj.py --match_endpoints 0 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 0 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_multiTraj" &
 srun --ntasks=1 python3 test_run_v6_hpc.py --match_endpoints 1 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 0 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_orig_matchEnds" &
