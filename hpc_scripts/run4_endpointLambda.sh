d3252700fc11160f142a6e979e26a8bd41a34c98
mattlevine22 <mattlevine22@gmail.com>
2022-02-15 15:30:55 -0800

    rerun endpointLambda experiments with learning rate plots

--- hpc_scripts/run4_endpointLambda.sh
+++ hpc_scripts/run4_endpointLambda.sh
@@ -13,7 +13,7 @@ module load gcc/9.2.0
 #conda init bash
 #conda activate fno
 
-round_dir="run4_endpointLambda"
+round_dir="run4_endpointLambda_v2"
 echo "Sending results to $round_dir"
 srun --ntasks=1 python3 test_run_v8_hpc.py --lambda_endpoints 0 --multi_traj 0 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 1 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_orig_knownIC" &
 srun --ntasks=1 python3 test_run_v8_hpc.py --lambda_endpoints 1 --multi_traj 0 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 0 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_orig_lam1" &
