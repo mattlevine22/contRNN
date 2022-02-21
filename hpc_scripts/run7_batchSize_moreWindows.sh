d897c605d3b3c26094e77e9faaf2b2fc6a0434f7
mattlevine22 <mattlevine22@gmail.com>
2022-02-21 22:23:49 +0100

    do ivan-style windows with warmup and no IC inference

--- hpc_scripts/run7_batchSize_moreWindows.sh
+++ hpc_scripts/run7_batchSize_moreWindows.sh
@@ -6,19 +6,19 @@
 #SBATCH --ntasks=6   # number of processor cores (i.e. tasks)
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=1G   # memory per CPU core
-#SBATCH -J "batchSizes_moreWins"   # job name
+#SBATCH -J "v2batchSizes_moreWins"   # job name
 
 module load python3/3.7.0
 module load gcc/9.2.0
 #conda init bash
 #conda activate fno
 
-round_dir="run7_batchSizes_moreWindows"
+round_dir="run7_batchSizes_moreWindows_warmup100"
 echo "Sending results to $round_dir"
-srun --ntasks=1 python3 test_run_v8_hpc.py --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 1 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_multiTraj_knownIC" &
-srun --ntasks=1 python3 test_run_v8_hpc.py --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 1000 --hpc 1 --known_inits 1 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs1000_multiTraj_knownIC" &
-srun --ntasks=1 python3 test_run_v8_hpc.py --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 10000 --hpc 1 --known_inits 1 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs10000_multiTraj_knownIC" &
-srun --ntasks=1 python3 test_run_v8_hpc.py --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 0 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_multiTraj_UNknownIC" &
-srun --ntasks=1 python3 test_run_v8_hpc.py --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 1000 --hpc 1 --known_inits 0 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs1000_multiTraj_UNknownIC" &
-srun --ntasks=1 python3 test_run_v8_hpc.py --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.1 --epochs 100000 --window 100 --use_f0 0 --do_normalization 1 --batch_size 10000 --hpc 1 --known_inits 0 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs10000_multiTraj_UNknownIC"
+srun --ntasks=1 python3 test_run_v8_hpc.py --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.1 --epochs 100000 --window 200 --warmup 100 --infer_ic 0 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 1 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_multiTraj_knownIC" &
+srun --ntasks=1 python3 test_run_v8_hpc.py --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.1 --epochs 100000 --window 200 --warmup 100 --infer_ic 0 --use_f0 0 --do_normalization 1 --batch_size 1000 --hpc 1 --known_inits 1 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs1000_multiTraj_knownIC" &
+srun --ntasks=1 python3 test_run_v8_hpc.py --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.1 --epochs 100000 --window 200 --warmup 100 --infer_ic 0 --use_f0 0 --do_normalization 1 --batch_size 10000 --hpc 1 --known_inits 1 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs10000_multiTraj_knownIC" &
+srun --ntasks=1 python3 test_run_v8_hpc.py --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.1 --epochs 100000 --window 200 --warmup 100 --infer_ic 0 --use_f0 0 --do_normalization 1 --batch_size 100 --hpc 1 --known_inits 0 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs100_multiTraj_UNknownIC" &
+srun --ntasks=1 python3 test_run_v8_hpc.py --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.1 --epochs 100000 --window 200 --warmup 100 --infer_ic 0 --use_f0 0 --do_normalization 1 --batch_size 1000 --hpc 1 --known_inits 0 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs1000_multiTraj_UNknownIC" &
+srun --ntasks=1 python3 test_run_v8_hpc.py --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.1 --epochs 100000 --window 200 --warmup 100 --infer_ic 0 --use_f0 0 --do_normalization 1 --batch_size 10000 --hpc 1 --known_inits 0 --dim_hidden 50 --n_layers 2 --activation gelu --output_dir "$round_dir/small_gelu_bs10000_multiTraj_UNknownIC"
 wait
