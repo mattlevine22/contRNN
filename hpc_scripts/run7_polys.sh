b4d0f19850e288fc4cc1c2845ed2634389218f6d
mattlevine22 <mattlevine22@gmail.com>
2022-02-22 21:28:52 +0100

    unscale bilinear layer...hopefully things don't blow up

--- hpc_scripts/run7_polys.sh
+++ hpc_scripts/run7_polys.sh
@@ -13,7 +13,7 @@ module load gcc/9.2.0
 #conda init bash
 #conda activate fno
 
-round_dir="run7_batchSizes_moreWindows_warmup100_initLR0.01_polys"
+round_dir="run7_batchSizes_moreWindows_warmup100_initLR0.01_polys_unscaled"
 echo "Sending results to $round_dir"
 srun --ntasks=1 python3 test_run_v8_hpc.py --plot_interval 10 --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.01 --epochs 100000 --window 200 --warmup 100 --infer_ic 0 --use_f0 0 --do_normalization 1 --batch_size 1000 --hpc 1 --known_inits 1 --dim_hidden 3 --n_layers 1 --output_dir "$round_dir/poly_bs1000_multiTraj_knownIC" &
 srun --ntasks=1 python3 test_run_v8_hpc.py --plot_interval 10 --max_grad_norm 1 --gpu 1 --min_lr 0.0001 --lambda_endpoints 0 --multi_traj 1 --lr 0.01 --epochs 100000 --window 200 --warmup 100 --infer_ic 0 --use_f0 0 --do_normalization 1 --batch_size 10000 --hpc 1 --known_inits 1 --dim_hidden 3 --n_layers 1 --output_dir "$round_dir/poly_bs10000_multiTraj_knownIC" &
