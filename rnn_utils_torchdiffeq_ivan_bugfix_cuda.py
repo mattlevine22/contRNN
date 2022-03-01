900df10b1909da8c7354856b9c602950f35d4f84
mattlevine22 <mattlevine22@gmail.com>
2022-03-01 10:00:13 +0100

    try/catch test_plots bc they seem to be failing on early runs due to integration issues.

--- rnn_utils_torchdiffeq_ivan_bugfix_cuda.py
+++ rnn_utils_torchdiffeq_ivan_bugfix_cuda.py
@@ -551,11 +551,13 @@ def train_model(model,
                     T_long = 5e3
 
                 f_path = os.path.join(plot_dir,'ContinueTraining_Epoch{}'.format(ep))
-                # try:
-                if gpu:
-                    test_plots(x0=u0.reshape(1,-1).cuda(), rhs_nn=model, nn_normalizer=x_normalizer, sol_3d_true=X_validation, T_long=T_long, output_path=f_path, logger=logger, gpu=gpu)
-                else:
-                    test_plots(x0=u0.reshape(1,-1), rhs_nn=model.rhs_numpy, nn_normalizer=x_normalizer, sol_3d_true=X_validation, T_long=T_long, output_path=f_path, logger=logger, gpu=gpu)
+                try:
+                    if gpu:
+                        test_plots(x0=u0.reshape(1,-1).cuda(), rhs_nn=model, nn_normalizer=x_normalizer, sol_3d_true=X_validation, T_long=T_long, output_path=f_path, logger=logger, gpu=gpu)
+                    else:
+                        test_plots(x0=u0.reshape(1,-1), rhs_nn=model.rhs_numpy, nn_normalizer=x_normalizer, sol_3d_true=X_validation, T_long=T_long, output_path=f_path, logger=logger, gpu=gpu)
+                except:
+                    print('Test Plots failed.')
 
     return model
 
