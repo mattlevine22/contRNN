28421b23cf16940addc069a2f3372bcd754bd477
mattlevine22 <mattlevine22@gmail.com>
2022-03-21 23:31:09 -0700

    adding code for direct approximation of L96MS right-hand-side

--- hpc_scripts/direct96_v0.sh
+++ hpc_scripts/direct96_v0.sh
@@ -3,7 +3,7 @@
 #Submit this script with: sbatch thefilename
 
 #SBATCH --time=48:00:00   # walltime
-#SBATCH --ntasks=7   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=1G   # memory per CPU core
 #SBATCH -J "directL96_v0"   # job name
@@ -16,10 +16,10 @@ module load gcc/9.2.0
 round_dir="experiments/directL96_v0"
 echo "Sending results to $round_dir"
 srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 1 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 10000 --hpc 1 --dim_hidden 500 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu500_layers2_gpu_bs10000" &
-srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 1 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 500 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu500_layers2_gpu_bs1000" &
-srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 0 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 2500 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu2500_layers2_cpu_bs1000" &
-srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 1 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 2500 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu2500_layers2_gpu_bs1000" &
-srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 0 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 500 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu500_layers2_cpu_bs1000" &
-srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 1 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 500 --n_layers 3 --activation gelu --output_dir "$round_dir/gelu500_layers3_gpu_bs1000" &
-srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 1 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 10000 --hpc 1 --dim_hidden 500 --n_layers 3 --activation gelu --output_dir "$round_dir/gelu500_layers3_gpu_bs10000"
+srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 1 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 500 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu500_layers2_gpu_bs1000"
+# srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 0 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 2500 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu2500_layers2_cpu_bs1000" &
+# srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 1 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 2500 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu2500_layers2_gpu_bs1000" &
+# srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 0 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 500 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu500_layers2_cpu_bs1000" &
+# srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 1 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 500 --n_layers 3 --activation gelu --output_dir "$round_dir/gelu500_layers3_gpu_bs1000" &
+# srun --ntasks=1 python3 train_l96_hpc.py --plot_interval 10 --gpu 1 --lr 0.01 --epochs 1000 --do_normalization 1 --batch_size 10000 --hpc 1 --dim_hidden 500 --n_layers 3 --activation gelu --output_dir "$round_dir/gelu500_layers3_gpu_bs10000"
 wait
