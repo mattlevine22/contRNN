cfe03c1d6574f93436beec71e147ccd821966a52
mattlevine22 <mattlevine22@gmail.com>
2022-03-25 15:00:47 -0700

    new gpu job

--- hpc_scripts/l96run_v2_0.sh
+++ hpc_scripts/l96run_v2_0.sh
@@ -3,7 +3,7 @@
 #Submit this script with: sbatch thefilename
 
 #SBATCH --time=24:00:00   # walltime
-#SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=10G   # memory per CPU core
 #SBATCH -J "l96_partial"   # job name
@@ -15,6 +15,5 @@ module load gcc/9.2.0
 
 round_dir="experiments/partialObs_eps-1_v0"
 echo "Sending results to $round_dir"
-srun -l python3 test_run_v9_hpc.py --eval_time_limit 6000 --adjoint 0 --ds_name L96Meps-1 --warmup_type 3dvar --cheat_normalization 0 --obs_noise_sd 0.01 --dim_x 9 --dim_y 72 --plot_interval 1 --max_grad_norm 1 --gpu 1 --multi_traj 1 --lr 0.01 --epochs 1000 --window 110 --warmup 100 --infer_ic 0 --use_f0 1 --do_normalization 1 --batch_size 1000 --hpc 1 --known_inits 0 --dim_hidden 2500 --n_layers 2 --activation gelu --output_dir "$round_dir/noise0.01_3dvar_T0.1_warmupT1_bs1000_multiTRAJ_dY72" &
-srun -l python3 test_run_v9_hpc.py --eval_time_limit 6000 --adjoint 1 --ds_name L96Meps-1 --warmup_type 3dvar --cheat_normalization 0 --obs_noise_sd 0.01 --dim_x 9 --dim_y 72 --plot_interval 1 --max_grad_norm 1 --gpu 1 --multi_traj 1 --lr 0.01 --epochs 1000 --window 110 --warmup 100 --infer_ic 0 --use_f0 1 --do_normalization 1 --batch_size 1000 --hpc 1 --known_inits 0 --dim_hidden 2500 --n_layers 2 --activation gelu --output_dir "$round_dir/noise0.01_3dvar_T0.1_warmupT1_bs1000_multiTRAJ_dY72_adjoint"
+srun -l python3 test_run_v9_hpc.py --T_long 500 --plot_interval 10 --eval_time_limit 10000 --adjoint 1 --ds_name L96Meps-1 --warmup_type 3dvar --cheat_normalization 0 --obs_noise_sd 0.01 --dim_x 9 --dim_y 72 --max_grad_norm 1 --gpu 1 --multi_traj 1 --lr 0.01 --epochs 1000 --window 110 --warmup 100 --infer_ic 0 --use_f0 1 --do_normalization 1 --batch_size 1000 --hpc 1 --known_inits 0 --dim_hidden 2500 --n_layers 2 --activation gelu --output_dir "$round_dir/noise0.01_3dvar_T0.1_warmupT1_bs1000_multiTRAJ_dY72_adjoint"
 wait
