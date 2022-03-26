fc14d749bfb90524d14152138d976b459db814f9
mattlevine22 <mattlevine22@gmail.com>
2022-03-25 20:00:20 -0700

    longer direct 96 training runs

--- hpc_scripts/direct96_v3_gpu.sh
+++ hpc_scripts/direct96_v3_gpu.sh
@@ -15,6 +15,6 @@ module load gcc/9.2.0
 
 round_dir="experiments/directL96_v1.3"
 echo "Sending results to $round_dir"
-srun --ntasks=1 python3 train_l96_hpc.py --patience 5 --factor 0.1 --T_long 500 --eval_time_limit 6000 --max_grad_norm 1 --plot_interval 100 --gpu 1 --lr 0.01 --epochs 50000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 2500 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu2500_layers2_gpu_bs1000_gradclip1_factor0.1_patience5" &
-srun --ntasks=1 python3 train_l96_hpc.py --patience 5 --factor 0.1 --T_long 500 --eval_time_limit 6000 --max_grad_norm 1 --plot_interval 100 --gpu 1 --lr 0.01 --epochs 50000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 1000 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu1000_layers2_gpu_bs1000_gradclip1_factor0.1_patience5" &
+srun --ntasks=1 python3 train_l96_hpc.py --patience 5 --factor 0.1 --T_long 500 --eval_time_limit 6000 --max_grad_norm 1 --plot_interval 500 --gpu 1 --lr 0.01 --epochs 50000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 2500 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu2500_layers2_gpu_bs1000_gradclip1_factor0.1_patience5" &
+srun --ntasks=1 python3 train_l96_hpc.py --patience 5 --factor 0.1 --T_long 500 --eval_time_limit 6000 --max_grad_norm 1 --plot_interval 500 --gpu 1 --lr 0.01 --epochs 50000 --do_normalization 1 --batch_size 1000 --hpc 1 --dim_hidden 1000 --n_layers 2 --activation gelu --output_dir "$round_dir/gelu1000_layers2_gpu_bs1000_gradclip1_factor0.1_patience5" &
 wait
